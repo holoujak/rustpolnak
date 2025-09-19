b2f182e72d9cfdcd7fde91aad164bf9190e402cf
holoujak <jakub.holoubek@gmail.com>
2025-09-19 22:52:14 +0200

    Use table_rs to show list of races

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![allow(dead_code)]
 
 use dioxus::prelude::*;
+use maplit::hashmap;
+use table_rs::dioxus::table::Table;
+use table_rs::dioxus::types::Column;
 
 use crate::restclient::RaceRestAPI;
 
@@ -24,14 +27,45 @@ fn App() -> Element {
         api.races().await
     });
 
+    let columns = vec![
+        Column {
+            id: "id",
+            header: "ID",
+            sortable: true,
+            ..Default::default()
+        },
+        Column {
+            id: "name",
+            header: "Name",
+            sortable: true,
+            ..Default::default()
+        },
+        Column {
+            id: "date_of_event",
+            header: "Date",
+            sortable: true,
+            ..Default::default()
+        },
+    ];
+
     rsx! {
         document::Link { rel: "stylesheet", href: MAIN_CSS }
         "Vyhlal!",
+
         match &*races.read() {
-            Some(Ok(races)) => rsx!{
-                ul {
-                    for race in races {
-                        li {"{race.name}"}
+            Some(Ok(races)) => {
+                let data: Vec<_> = races.iter().map(|race| {
+                    hashmap! {
+                        "id" => race.id.to_string(),
+                        "name" => race.name.clone(),
+                        "date_of_event" => race.date_of_event.format("%d.%m.%Y").to_string(),
+                    }
+                }).collect();
+
+                rsx! {
+                    Table {
+                        columns: columns,
+                        data: data,
                     }
                 }
             },
