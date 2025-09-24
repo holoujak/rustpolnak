65c4f4915d2731df98b5b0bbf2f7f65ffc6d9161
Jakub Holoubek <jakub.holoubek@gmail.com>
2025-09-24 13:31:28 +0200

    Add example table for races

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 #![allow(dead_code)]
 
 use dioxus::prelude::*;
-use maplit::hashmap;
-use table_rs::dioxus::table::Table;
-use table_rs::dioxus::types::Column;
 
 use crate::restclient::RaceRestAPI;
 
@@ -25,28 +22,7 @@ fn App() -> Element {
     let races = use_resource(move || async move {
         let api = use_context::<RaceRestAPI>();
         api.races().await
-    });
-
-    let columns = vec![
-        Column {
-            id: "id",
-            header: "ID",
-            sortable: true,
-            ..Default::default()
-        },
-        Column {
-            id: "name",
-            header: "Name",
-            sortable: true,
-            ..Default::default()
-        },
-        Column {
-            id: "date_of_event",
-            header: "Date",
-            sortable: true,
-            ..Default::default()
-        },
-    ];
+    }); 
 
     rsx! {
         document::Link { rel: "stylesheet", href: MAIN_CSS }
@@ -54,18 +30,24 @@ fn App() -> Element {
 
         match &*races.read() {
             Some(Ok(races)) => {
-                let data: Vec<_> = races.iter().map(|race| {
-                    hashmap! {
-                        "id" => race.id.to_string(),
-                        "name" => race.name.clone(),
-                        "date_of_event" => race.date_of_event.format("%d.%m.%Y").to_string(),
-                    }
-                }).collect();
-
                 rsx! {
-                    Table {
-                        columns: columns,
-                        data: data,
+                    table {
+                        thead {
+                            tr {
+                                th { "Id" }
+                                th { "Name" }
+                                th { "Date" }
+                            }
+                        }
+                        tbody {
+                            for race in races.iter() {
+                                tr {
+                                    td { "{race.id}" }
+                                    td { "{race.name}" }
+                                    td { "{race.date_of_event}" }
+                                }
+                            }
+                        }
                     }
                 }
             },
