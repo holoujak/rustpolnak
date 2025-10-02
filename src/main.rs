320a581da6588ca8aa3d6e99633ebc5915d77998
Daniel Trnka <daniel.trnka@gmail.com>
2025-10-02 23:50:39 +0200

    disable menu on release builds

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 
+use dioxus::desktop::{Config, LogicalSize, WindowBuilder};
 use dioxus::prelude::*;
 
 use crate::restclient::RaceRestAPI;
@@ -13,8 +14,29 @@ mod sorter;
 
 const MAIN_CSS: Asset = asset!("/assets/main.css");
 
+fn appconfig_default() -> Config {
+    Config::default().with_window(
+        WindowBuilder::new()
+            .with_maximized(false)
+            .with_title("rustpolnak")
+            .with_min_inner_size(LogicalSize::new(1280, 768)),
+    )
+}
+
+#[cfg(debug_assertions)]
+fn appconfig() -> Config {
+    appconfig_default()
+}
+
+#[cfg(not(debug_assertions))]
+fn appconfig() -> Config {
+    appconfig_default()
+        .with_menu(None)
+        .with_disable_context_menu(true)
+}
+
 fn main() {
-    dioxus::launch(App);
+    LaunchBuilder::new().with_cfg(appconfig()).launch(App);
 }
 
 #[component]
