56a824fb61ef7b373e29f2ae430c9fee868d741c
holoujak <jakub.holoubek@gmail.com>
2025-10-02 21:41:48 +0200

    Load registrations base on selected race

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use dioxus::desktop::{Config, LogicalSize, WindowBuilder};
 use dioxus::prelude::*;
 
 use crate::restclient::RaceRestAPI;
-use crate::restclient::{Race, RaceField};
+use crate::restclient::{Racer, RacerField};
 use crate::sort_table::Th;
 use crate::sorter::Sorter;
 
@@ -46,39 +46,39 @@ fn App() -> Element {
         RaceRestAPI::new(server_url, "username", "password")
     });
 
+    let selected_race_id = use_signal(|| Option::<u32>::None);
+
+    rsx! {
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        div {
+            RacesList{ selected_race_id }
+            if let Some(race_id) = *selected_race_id.read() {
+                Registrations{ race_id }
+            }
+
+        }
+    }
+}
+
+#[component]
+fn RacesList(selected_race_id: Signal<Option<u32>>) -> Element {
     let races = use_resource(move || async move {
         let api = use_context::<RaceRestAPI>();
         api.races().await
     });
 
-    let sorter = use_signal(|| Sorter::<RaceField>::new(RaceField::Id));
-
     rsx! {
-        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        "Races:",
         match &*races.read() {
-            Some(Ok(races)) => {
-                let mut sorted = (*races).clone();
-                let field = sorter.read().active;
-                sorted.sort_by(|a, b| sorter.read().cmp_by(a, b, field, Race::cmp_by));
-
-                rsx! {
-                    table {
-                        thead {
-                            tr {
-                                Th { sorter, field: RaceField::Id, "ID" }
-                                Th { sorter, field: RaceField::Name, "Name" }
-                                Th { sorter, field: RaceField::DateOfEvent, "Date" }
-                            }
-                        }
-                        tbody {
-                            for race in sorted.iter() {
-                                tr {
-                                    td { "{race.id}" }
-                                    td { "{race.name}" }
-                                    td { "{race.date_of_event}" }
-                                }
-                            }
-                        }
+            Some(Ok(races)) => rsx! {
+                select {
+                    onchange: move |e| {
+                        let val = e.value().parse::<u32>().ok();
+                        selected_race_id.set(val);
+                    },
+                    option { disabled: true, selected: true, "Select race" }
+                    for race in races.iter() {
+                        option { value: "{race.id}", "{race.name}" }
                     }
                 }
             },
@@ -88,4 +88,56 @@ fn App() -> Element {
             _ => rsx!{}
         }
     }
+}
+
+#[component]
+fn Registrations(race_id: ReadOnlySignal<u32>) -> Element {
+    let registrations = use_resource(move || async move {
+        let api = use_context::<RaceRestAPI>();
+        api.registrations(*race_id.read()).await
+    });
+
+    let sorter = use_signal(|| Sorter::<RacerField>::new(RacerField::StartNumber));
+
+    let x = match &*registrations.read() {
+        Some(Ok(racer)) => {
+            let mut sorted = (*racer).clone();
+            let field = sorter.read().active;
+            sorted.sort_by(|a, b| sorter.read().cmp_by(a, b, field, Racer::cmp_by));
+
+            rsx! {
+                table {
+                    thead {
+                        tr {
+                            Th { sorter, field: RacerField::StartNumber, "Start number" }
+                            Th { sorter, field: RacerField::FirstName, "First name" }
+                            Th { sorter, field: RacerField::LastName, "Last name" }
+                            Th { sorter, field: RacerField::Track, "Track" }
+                            th { "Categories" }
+                        }
+                    }
+                    tbody {
+                        for racer in sorted.iter() {
+                            tr {
+                                td { "{racer.start_number.unwrap_or(0)}" }
+                                td { "{racer.first_name}" }
+                                td { "{racer.last_name}" }
+                                td { "{racer.track.name}" }
+                                td {
+                                    for category in racer.categories.clone() {
+                                        "{category.name}, "
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        Some(Err(err)) => rsx! {
+            div {"{err:?}"}
+        },
+        _ => rsx! {},
+    };
+    x
 }
