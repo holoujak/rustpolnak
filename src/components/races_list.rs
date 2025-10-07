838f47a8f50aab38782374e436aa274833e0e3a6
Daniel Trnka <daniel.trnka@gmail.com>
2025-10-08 00:41:33 +0200

    races_list: autoload the earliest race

--- src/components/races_list.rs
+++ src/components/races_list.rs
@@ -4,15 +4,24 @@ use dioxus::prelude::*;
 
 use crate::{race::Race, restclient::RaceRestAPI};
 
+type SignalRace = Signal<Option<Result<Race, Box<dyn std::error::Error>>>>;
+
+async fn load_race(mut selected_race: SignalRace, id: u32) {
+    let race = Race::load(use_context::<RaceRestAPI>(), id).await;
+    selected_race.set(Some(race));
+}
+
 #[component]
-pub fn RacesList(
-    selected_race: Signal<Option<Result<Race, Box<dyn std::error::Error>>>>,
-) -> Element {
+pub fn RacesList(selected_race: SignalRace) -> Element {
     let races: Resource<Result<Vec<crate::restclient::Race>, Box<dyn Error>>> =
         use_resource(move || async move {
             let api = use_context::<RaceRestAPI>();
             let mut races = api.races().await?;
             races.sort_by(|a, b| b.date_of_event.cmp(&a.date_of_event));
+
+            if let Some(earliest_race) = races.first() {
+                load_race(selected_race, earliest_race.id).await;
+            }
             Ok(races)
         });
 
@@ -22,13 +31,12 @@ pub fn RacesList(
                 select {
                     class: "form-select mb-1",
                     onchange: move |e| {
+                        e.prevent_default();
                         let race_id = e.value().parse::<u32>().ok().unwrap();
                         spawn(async move {
-                            let race = Race::load(use_context::<RaceRestAPI>(), race_id).await;
-                            selected_race.set(Some(race));
+                            load_race(selected_race, race_id).await;
                         });
                     },
-                    option { disabled: true, selected: true, "Select race" }
                     for race in races.iter() {
                         option { value: "{race.id}", "{race.name}" }
                     }
