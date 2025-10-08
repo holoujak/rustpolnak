4d14dae6a5979296ee500534f730f4f8807ee062
Daniel Trnka <daniel.trnka@gmail.com>
2025-10-08 22:34:14 +0200

    app: extract rfid and action handling into functions

--- src/components/app.rs
+++ src/components/app.rs
@@ -10,6 +10,8 @@ use crate::{
     rfid_reader,
 };
 
+type SelectedRace = Option<Result<Race, Box<dyn std::error::Error>>>;
+
 const LOADING: Asset = asset!("/assets/loading.webp");
 
 #[derive(Debug)]
@@ -18,9 +20,45 @@ pub enum Action {
     FinishByStartNumber(u32),
 }
 
+fn handle_rfid_event(selected_race: &mut Signal<SelectedRace>, event: rfid_reader::Event) {
+    match event {
+        rfid_reader::Event::Connected(device) => info!("RFID {device} connected"),
+        rfid_reader::Event::Disconnected { device, error } => {
+            info!("RFID {device} disconnected: {error:?}")
+        }
+        rfid_reader::Event::Tag(tag) => {
+            info!("Tag {tag}");
+            selected_race.with_mut(|maybe_race| {
+                if let Some(Ok(race)) = maybe_race {
+                    race.tag_finished(&tag);
+                }
+            });
+        }
+    }
+}
+
+fn handle_action(selected_race: &mut Signal<SelectedRace>, action: Action) {
+    match action {
+        Action::Start(track) => {
+            selected_race.with_mut(|maybe_race| {
+                if let Some(Ok(race)) = maybe_race {
+                    race.start(track);
+                }
+            });
+        }
+        Action::FinishByStartNumber(starting_number) => {
+            selected_race.with_mut(|maybe_race| {
+                if let Some(Ok(race)) = maybe_race {
+                    race.finish_start_number(starting_number);
+                }
+            });
+        }
+    }
+}
+
 #[component]
 pub fn App() -> Element {
-    let mut selected_race = use_signal(|| Option::<Result<Race, Box<dyn std::error::Error>>>::None);
+    let mut selected_race = use_signal(|| SelectedRace::None);
 
     use_coroutine(
         move |mut actions_rx: UnboundedReceiver<Action>| async move {
@@ -33,38 +71,11 @@ pub fn App() -> Element {
                 tokio::select! {
                     Ok(rfid_event) = rfid_rx.recv() => {
                         println!("{rfid_event:?}");
-                        match rfid_event {
-                            rfid_reader::Event::Connected(device) => info!("RFID {device} connected"),
-                            rfid_reader::Event::Disconnected { device, error } => info!("RFID {device} disconnected: {error:?}"),
-                            rfid_reader::Event::Tag(tag) => {
-                                info!("Tag {tag}");
-                                selected_race.with_mut(|maybe_race| {
-                                    if let Some(Ok(race)) = maybe_race {
-                                        race.tag_finished(&tag);
-                                    }
-                                });
-                            },
-                        }
+                       handle_rfid_event(&mut selected_race, rfid_event);
                     }
-
-                    Some(msg) = actions_rx.next() => {
-                        println!("{msg:?}");
-                        match msg {
-                            Action::Start(track) => {
-                                selected_race.with_mut(|maybe_race| {
-                                    if let Some(Ok(race)) = maybe_race {
-                                        race.start(track);
-                                    }
-                                });
-                            }
-                            Action::FinishByStartNumber(starting_number) => {
-                                selected_race.with_mut(|maybe_race| {
-                                    if let Some(Ok(race)) = maybe_race {
-                                        race.finish_start_number(starting_number);
-                                    }
-                                });
-                            }
-                        }
+                    Some(action) = actions_rx.next() => {
+                        println!("{action:?}");
+                        handle_action(&mut selected_race, action);
                     }
                 }
             }
