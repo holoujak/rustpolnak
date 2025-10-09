d68891a151024f907eedd8852d6f16374b1644dd
Jakub Holoubek <jakub.holoubek@gmail.com>
2025-10-09 23:23:12 +0200

    Add racers filter inputs

--- src/sort_table.rs
+++ src/sort_table.rs
@@ -7,6 +7,7 @@ pub struct ThProps<F: Copy + Eq + std::hash::Hash + 'static> {
     pub sorter: Signal<Sorter<F>>,
     pub field: F,
     #[props(optional)]
+    pub filters: Option<Signal<std::collections::HashMap<F, String>>>,
     pub children: Element,
 }
 
@@ -14,15 +15,18 @@ pub struct ThProps<F: Copy + Eq + std::hash::Hash + 'static> {
 #[allow(non_snake_case)]
 pub fn Th<F: Copy + Eq + std::hash::Hash + 'static>(props: ThProps<F>) -> Element {
     let mut sorter = props.sorter;
+    let filters = props.filters;
     let field = props.field;
 
     rsx! {
-        th {
-            role: "button",
-            onclick: move |_| {
-                sorter.write().toggle(field);
-            },
-            {props.children}
+        th { role: "button",
+            span {
+                onclick: move |_| {
+                    sorter.write().toggle(field);
+                },
+                {props.children}
+            }
+
             {
                 let s = sorter();
                 if s.active == field {
@@ -38,6 +42,26 @@ pub fn Th<F: Copy + Eq + std::hash::Hash + 'static>(props: ThProps<F>) -> Elemen
                     rsx! {
                         span { class: "invisible", " ↑" }
                     }
+                }
+            }
+            {
+                if let Some(mut filters_signal) = filters {
+                    rsx! {
+                        div { class: "mt-1",
+                            input {
+                                r#type: "text",
+                                placeholder: "Filter",
+                                // read current value for this field from the map
+                                value: filters_signal().get(&field).map(|s| s.as_str()).unwrap_or(""),
+                                oninput: move |e| {
+                                    let v = e.value().trim().to_string();
+                                    filters_signal.write().insert(field, v);
+                                },
+                            }
+                        }
+                    }
+                } else {
+                    rsx! {}
                 }
             }
         }
