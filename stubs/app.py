2e1646886e29498247aa2c91ccdffc8b9f5482a7
Daniel Trnka <daniel.trnka@gmail.com>
2025-10-10 19:02:31 +0200

    api stub: use pydantic

--- stubs/app.py
+++ stubs/app.py
@@ -1,85 +1,107 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional, List
 
 app = FastAPI()
 
 
+class Event(BaseModel):
+    id: int
+    name: str
+    description: str
+    dateOfEvent: str
+
+
+class Category(BaseModel):
+    id: int
+    name: str
+    description: Optional[str]
+
+
+class Track(BaseModel):
+    id: int
+    name: str
+    description: str
+
+
+class Racer(BaseModel):
+    id: int
+    firstName: str
+    lastName: str
+    startNumber: Optional[int]
+    categories: List[Category]
+    tagId: Optional[str]
+    track: Track
+
+
+class Result(BaseModel):
+    registrationId: int
+    startTime: str
+    finishTime: str
+
+
+class RaceResult(BaseModel):
+    results: List[Result]
+
+
 @app.get("/races")
-def races():
+def races() -> List[Event]:
     return [
-        {
-            "id": 1,
-            "name": "Přespolňák 2019",
-            "description": "Již čtvrtý ročník oblíbeného přespolního běhu",
-            "dateOfEvent": "2019-10-05 10:00:00.000000",
-        },
-        {
-            "id": 2,
-            "name": "Přespolňák 2020",
-            "description": "Již paty ročník oblíbeného přespolního běhu",
-            "dateOfEvent": "2020-10-05 10:00:00.000000",
-        },
-        {
-            "id": 3,
-            "name": "Přespolňák 2021",
-            "description": "Již sesty ročník oblíbeného přespolního běhu",
-            "dateOfEvent": "2021-10-05 10:00:00.000000",
-        },
+        Event(
+            id=1,
+            name="Přespolňák 2019",
+            description="Již čtvrtý ročník oblíbeného přespolního běhu",
+            dateOfEvent="2019-10-05 10:00:00.000000",
+        ),
+        Event(
+            id=2,
+            name="Přespolňák 2020",
+            description="Již paty ročník oblíbeného přespolního běhu",
+            dateOfEvent="2020-10-05 10:00:00.000000",
+        ),
+        Event(
+            id=3,
+            name="Přespolňák 2021",
+            description="Již sesty ročník oblíbeného přespolního běhu",
+            dateOfEvent="2021-10-05 10:00:00.000000",
+        ),
     ]
 
 
 @app.get("/races/{race_id}/registrations")
-def registrations(race_id: int):
+def registrations(race_id: int) -> List[Racer]:
     return [
-        {
-            "id": 1,
-            "firstName": "John",
-            "lastName": "Doe",
-            "startNumber": None,
-            "categories": [
-                {
-                    "id": 92,
-                    "name": "A10",
-                    "description": None,
-                }
-            ],
-            "tagId": None,
-            "track": {"id": 25, "name": "10 Km", "description": ""},
-        },
-        {
-            "id": 2,
-            "firstName": "Carlos",
-            "lastName": "Smith",
-            "startNumber": 123,
-            "categories": [
-                {
-                    "id": 92,
-                    "name": "A10",
-                    "description": None,
-                }
-            ],
-            "tagId": None,
-            "track": {"id": 25, "name": "10 Km", "description": ""},
-        },
-        {
-            "id": 3,
-            "firstName": "Oioioi",
-            "lastName": "Boi",
-            "startNumber": 124,
-            "categories": [
-                {
-                    "id": 93,
-                    "name": "B4",
-                    "description": None,
-                }
-            ],
-            "tagId": None,
-            "track": {"id": 25, "name": "4 Km", "description": ""},
-        },
+        Racer(
+            id=1,
+            firstName="John",
+            lastName="Doe",
+            startNumber=None,
+            categories=[Category(id=92, name="A10", description=None)],
+            tagId="1234",
+            track=Track(id=25, name="10 Km", description=""),
+        ),
+        Racer(
+            id=2,
+            firstName="Carlos",
+            lastName="Smith",
+            startNumber=123,
+            categories=[Category(id=92, name="A10", description=None)],
+            tagId="1111",
+            track=Track(id=25, name="10 Km", description=""),
+        ),
+        Racer(
+            id=3,
+            firstName="Oioioi",
+            lastName="Boi",
+            startNumber=124,
+            categories=[Category(id=93, name="B4", description=None)],
+            tagId=None,
+            track=Track(id=25, name="4 Km", description=""),
+        ),
     ]
 
 
 @app.post("/races/{race_id}/results")
-async def results(race_id: int, request: Request):
-    data = await request.json()
-    print(data)
+def results(race_id: int, results: RaceResult):
+    print(results)
     return {}
