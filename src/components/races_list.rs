856234cd5ea3856e5107dc98a4fce16dacd3dfa9
Daniel Trnka <daniel.trnka@gmail.com>
2025-10-10 21:02:47 +0200

    dont get RaceRestAPI from context outside components

--- src/components/races_list.rs
+++ src/components/races_list.rs
@@ -6,24 +6,28 @@ use crate::{race::Race, restclient::RaceRestAPI};
 
 type SignalRace = Signal<Option<Result<Race, Box<dyn std::error::Error>>>>;
 
-async fn load_race(mut selected_race: SignalRace, id: u32) {
+async fn load_race(api: RaceRestAPI, mut selected_race: SignalRace, id: u32) {
     selected_race.set(None);
-    let race = Race::load(use_context::<RaceRestAPI>(), id).await;
+    let race = Race::load(api, id).await;
     selected_race.set(Some(race));
 }
 
 #[component]
 pub fn RacesList(selected_race: SignalRace) -> Element {
-    let races: Resource<Result<Vec<crate::restclient::Race>, Box<dyn Error>>> =
-        use_resource(move || async move {
-            let api = use_context::<RaceRestAPI>();
-            let mut races = api.races().await?;
-            races.sort_by(|a, b| b.date_of_event.cmp(&a.date_of_event));
+    let api = use_context::<RaceRestAPI>();
 
-            if let Some(earliest_race) = races.first() {
-                load_race(selected_race, earliest_race.id).await;
+    let races: Resource<Result<Vec<crate::restclient::Race>, Box<dyn Error>>> =
+        use_resource(move || {
+            let api = api.clone();
+            async move {
+                let mut races = api.races().await?;
+                races.sort_by(|a, b| b.date_of_event.cmp(&a.date_of_event));
+
+                if let Some(earliest_race) = races.first() {
+                    load_race(api, selected_race, earliest_race.id).await;
+                }
+                Ok(races)
             }
-            Ok(races)
         });
 
     rsx! {
@@ -34,8 +38,9 @@ pub fn RacesList(selected_race: SignalRace) -> Element {
                     onchange: move |e| {
                         e.prevent_default();
                         let race_id = e.value().parse::<u32>().ok().unwrap();
+                        let api = use_context::<RaceRestAPI>();
                         spawn(async move {
-                            load_race(selected_race, race_id).await;
+                            load_race(api, selected_race, race_id).await;
                         });
                     },
                     for race in races.iter() {
