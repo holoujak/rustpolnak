2dd08d652f3720eafd1495120eafc8c199d7c752
Daniel Trnka <daniel.trnka@gmail.com>
2025-10-16 23:53:53 +0200

    track_start: countdown

--- src/components/track_start.rs
+++ src/components/track_start.rs
@@ -1,3 +1,6 @@
+use std::time::Duration;
+
+use crate::time_utils::format_time_delta_secs;
 use chrono::{DateTime, Utc};
 use dioxus::prelude::*;
 
@@ -10,13 +13,32 @@ use crate::{
 pub fn TrackStart(track: Track, start: Option<DateTime<Utc>>) -> Element {
     let mut editing = use_signal(|| false);
     let track2 = track.clone();
+    let time_since_start = use_signal(|| None);
+
+    use_hook(move || {
+        spawn({
+            to_owned![time_since_start];
+            async move {
+                loop {
+                    tokio::time::sleep(Duration::from_secs(1)).await;
+                    time_since_start
+                        .set(start.map(|start| Some(Utc::now().signed_duration_since(start))));
+                }
+            }
+        });
+    });
 
     rsx! {
-        div { class: "input-group", style: "width: 400px",
+        div { class: "input-group", style: "width: 490px",
             span {
                 class: "input-group-text justify-content-end",
                 style: "width: 150px",
                 "{track}"
+            }
+            span { class: "input-group-text", style: "width: 90px",
+                if let Some(time_since_start) = time_since_start() {
+                    "{format_time_delta_secs(time_since_start)}"
+                }
             }
             TimeInput {
                 time: start,
